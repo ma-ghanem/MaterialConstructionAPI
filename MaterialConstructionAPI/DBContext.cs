9b6e1bd61b4c428807f7fb7ff383a36e0538861a
Mohamed Ghanem <ma-ghanem@hotmail.com>
2017-08-10 02:16:42 +0200

    API Structure (#2)

---
+++ MaterialConstructionAPI/DBContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace MaterialConstructionAPI
+{
+    public class DBContext
+    {
+        string connetionString = null;
+        SqlConnection conn;
+
+        public DBContext()
+        {
+            InitConnection();
+        }
+
+        public void InitConnection()
+        {
+            connetionString = ConfigurationManager.ConnectionStrings["MaterialConstructionConnection"].ConnectionString;
+            conn = new SqlConnection(connetionString);
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return conn;
+        }
+
+        public void OpenConnection()
+        {
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                //("Can not open connection !");
+            }
+        }
+
+        public void CloseConnection()
+        {
+            try
+            {
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                //("Can not close connection !");
+            }
+        }
+    }
+}
